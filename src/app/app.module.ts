461209ac342a6ceb973efac42631241f837ab6f0
Nikita Drozhzhin <drozhzhin.n.e@mail.ru>
2019-03-27 09:19:12 +0300

    CLI project update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { TooltipModule } from './tooltip/tooltip.module';
+import { TooltipModule } from 'ng2-tooltip-directive';
+//import { TooltipModule } from './tooltip/tooltip.module';
 import { FirstPageComponent } from './first-page.component';
 import { SecondPageComponent } from './second-page.component';
 
